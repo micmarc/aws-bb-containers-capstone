e22005eabe6da2ab4d33224a68008a7836988f56
Michael Marcus <mmarcus@captechconsulting.com>
2022-06-25 10:20:37 -0400

    Project setup

---
+++ lib/aws-bb-containers-capstone-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class AwsBbContainersCapstoneStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'AwsBbContainersCapstoneQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
