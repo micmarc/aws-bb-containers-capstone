620a07406c11954107e8378327dea25b1fb871a7
Michael Marcus <mmarcus@captechconsulting.com>
2022-06-25 11:36:14 -0400

    CDK quickstart

--- bin/aws-bb-containers-capstone.ts
+++ bin/aws-bb-containers-capstone.ts
@@ -1,21 +1,26 @@
-#!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsBbContainersCapstoneStack } from '../lib/aws-bb-containers-capstone-stack';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
 
 const app = new cdk.App();
-new AwsBbContainersCapstoneStack(app, 'AwsBbContainersCapstoneStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const account = '219890958300';
+const region = 'us-east-1';
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const addOns: Array<blueprints.ClusterAddOn> = [
+    new blueprints.addons.ArgoCDAddOn,
+    new blueprints.addons.CalicoOperatorAddOn,
+    new blueprints.addons.MetricsServerAddOn,
+    new blueprints.addons.ClusterAutoScalerAddOn,
+    new blueprints.addons.ContainerInsightsAddOn,
+    new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+    new blueprints.addons.VpcCniAddOn(),
+    new blueprints.addons.CoreDnsAddOn(),
+    new blueprints.addons.KubeProxyAddOn(),
+    new blueprints.addons.XrayAddOn()
+];
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+blueprints.EksBlueprint.builder()
+    .account(account)
+    .region(region)
+    .addOns(...addOns)
+    .build(app, 'aws-bb-containers-capstone');
