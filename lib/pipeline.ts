16e0a78f61d8314836b4bc6c8b1d0b57a4a4c041
Michael Marcus <mmarcus@captechconsulting.com>
2022-07-28 14:31:23 -0400

    Add back test and prod

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -52,8 +52,8 @@ export default class PipelineConstruct extends Construct {
                 id: "envs",
                 stages: [
                     {id: "dev", stackBuilder: blueprint.clone('us-west-2').addOns(devBootstrapArgo)},
-                    // {id: "test", stackBuilder: blueprint.clone('us-east-2').addOns(testBootstrapArgo)},
-                    // {id: "prod", stackBuilder: blueprint.clone('us-east-1').addOns(prodBootstrapArgo)},
+                    {id: "test", stackBuilder: blueprint.clone('us-east-2').addOns(testBootstrapArgo)},
+                    {id: "prod", stackBuilder: blueprint.clone('us-east-1').addOns(prodBootstrapArgo)},
                 ],
             })
             .build(scope, id + "-stack", props);
